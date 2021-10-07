1aa60cf9977dd38322aec1b0932b7cff859b86c9
alexdev22 <alejandro2210lp@gmail.com>
2021-10-07 23:04:39 +0200

    connected database and created blueprint

--- server/database.js
+++ server/database.js
@@ -3,16 +3,8 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '112224',
-    database: 'book-review'
+    database: 'books_reviews'
 })
-
-connection.connect(function (err) {
-    if (err) {
-        return console.error('error: ' + err.message);
-    }
-
-    console.log('Connected to the MySQL server.');
-});
 
 
 module.exports = connection
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const connection = require('./database')
+const connection = require('./database.js')
 
 const app = express()
 
@@ -10,4 +10,11 @@ app.use(cors())
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
+
+
+
+connection.query('SELECT * FROM reviews', (err, results, fields) => {
+    console.log(results);
+})
+
 
